2518bb609683efe9384781688ca51f69f95a0889
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2023-03-30 12:18:43 +0200

    Some updates

--- scripts/containerlab/5gsa-ueransim-open5gs-deploy.sh
+++ scripts/containerlab/5gsa-ueransim-open5gs-deploy.sh
@@ -7,52 +7,51 @@ sudo containerlab deploy --topo ../../topologies/containerlab/5gsa-ueransim-open
 sudo docker cp ../../conf/ueransim/ue.yaml clab-5gsa-ueransim-open5gs-ue:/
 sudo docker cp ../../conf/ueransim/gnb.yaml clab-5gsa-ueransim-open5gs-gnb:/
 
-sudo docker exec -d clab-5gsa-ueransim-open5gs-mongodb mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongodb.log --bind_ip 0.0.0.0
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-mongodb mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongodb.log --bind_ip 0.0.0.0
 
 sudo docker cp ../../conf/open5gs/nrf.yaml clab-5gsa-ueransim-open5gs-nrf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-nrf /open5gs/install/bin/open5gs-nrfd -D -c /nrf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-nrf /open5gs/install/bin/open5gs-nrfd -c /nrf.yaml
 
 sudo docker cp ../../conf/open5gs/scp.yaml clab-5gsa-ueransim-open5gs-scp:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-scp /open5gs/install/bin/open5gs-scpd -D -c /scp.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-scp /open5gs/install/bin/open5gs-scpd -c /scp.yaml
 
 sudo docker cp ../../conf/open5gs/amf.yaml clab-5gsa-ueransim-open5gs-amf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-amf /open5gs/install/bin/open5gs-amfd -D -c /amf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-amf /open5gs/install/bin/open5gs-amfd -c /amf.yaml
 
 sudo docker cp ../../conf/open5gs/smf.yaml clab-5gsa-ueransim-open5gs-smf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-smf /open5gs/install/bin/open5gs-smfd -D -c /smf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-smf /open5gs/install/bin/open5gs-smfd -c /smf.yaml
 
 sudo docker cp ../../conf/open5gs/upf.yaml clab-5gsa-ueransim-open5gs-upf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-upf /open5gs/install/bin/open5gs-upfd -D -c /upf.yaml
-sleep 5
+sudo docker exec clab-5gsa-ueransim-open5gs-upf dhclient eth3
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip route del default
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip route add default via 192.168.122.1 dev eth3
+sudo docker exec clab-5gsa-ueransim-open5gs-upf sysctl -w net.ipv4.ip_forward=1
+sudo docker exec clab-5gsa-ueransim-open5gs-upf sysctl -w net.ipv6.conf.all.forwarding=1
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip tuntap add name ogstun mode tun
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip addr add 10.45.0.1/16 dev ogstun
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip addr add 2001:db8:cafe::1/48 dev ogstun
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip link set ogstun up
+sudo docker exec clab-5gsa-ueransim-open5gs-upf iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
+sudo docker exec clab-5gsa-ueransim-open5gs-upf ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE
+sudo docker exec -td clab-5gsa-ueransim-open5gs-upf /open5gs/install/bin/open5gs-upfd -c /upf.yaml
 
 sudo docker cp ../../conf/open5gs/ausf.yaml clab-5gsa-ueransim-open5gs-ausf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-ausf /open5gs/install/bin/open5gs-ausfd -D -c /ausf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-ausf /open5gs/install/bin/open5gs-ausfd -c /ausf.yaml
 
 sudo docker cp ../../conf/open5gs/udm.yaml clab-5gsa-ueransim-open5gs-udm:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-udm /open5gs/install/bin/open5gs-udmd -D -c /udm.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-udm /open5gs/install/bin/open5gs-udmd -c /udm.yaml
 
 sudo docker cp ../../conf/open5gs/pcf.yaml clab-5gsa-ueransim-open5gs-pcf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-pcf /open5gs/install/bin/open5gs-pcfd -D -c /pcf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-pcf /open5gs/install/bin/open5gs-pcfd -c /pcf.yaml
 
 sudo docker cp ../../conf/open5gs/nssf.yaml clab-5gsa-ueransim-open5gs-nssf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-nssf /open5gs/install/bin/open5gs-nssfd -D -c /nssf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-nssf /open5gs/install/bin/open5gs-nssfd -c /nssf.yaml
 
 sudo docker cp ../../conf/open5gs/bsf.yaml clab-5gsa-ueransim-open5gs-bsf:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-bsf /open5gs/install/bin/open5gs-bsfd -D -c /bsf.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-bsf /open5gs/install/bin/open5gs-bsfd -c /bsf.yaml
 
 sudo docker cp ../../conf/open5gs/udr.yaml clab-5gsa-ueransim-open5gs-udr:/
-sudo docker exec -d clab-5gsa-ueransim-open5gs-udr /open5gs/install/bin/open5gs-udrd -D -c /udr.yaml
-sleep 5
+sudo docker exec -td clab-5gsa-ueransim-open5gs-udr /open5gs/install/bin/open5gs-udrd -c /udr.yaml
 
 # For WebUI:
 # export DB_URI=mongodb://10.254.1.100/open5gs
